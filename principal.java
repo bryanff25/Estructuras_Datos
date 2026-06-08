7a801b36f0b6fc3357e6dc94bddd04775ca8331f
bryanff25 <bryankff25@gmail.com>
2026-06-08 07:57:49 -0600

    Create principal.java

---
+++ principal.java
@@ -0,0 +1,11 @@
+public class principal {
+    void main(){
+        funciones.saludar();
+        
+        System.out.println(funciones.calcularfactorial(5));
+
+        System.out.println(funciones.CalcularIVA(300));
+
+        System.out.println(funciones.calcularAreaCirculo(50));
+    }
+}
